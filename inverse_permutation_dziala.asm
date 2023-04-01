351d84a8bd04eefcc45c20e88884182f3e81f8cb
AnthonyGrod <53835371+AnthonyGrod@users.noreply.github.com>
2023-04-01 22:09:04 +0200

    prawie kom ang

--- inverse_permutation_dziala.asm
+++ inverse_permutation_dziala.asm
@@ -1,17 +1,17 @@
 global inverse_permutation
 
-; rdi - wartość n (64 bity)
-; rsi - wartość wskaźnika na początek tablicy p (64 bity)
+; rdi - value n passed as first argument
+; rsi - pointer to the arrray p passed as second argument
 
 inverse_permutation:
-         test    rdi, rdi             ; Sprawdzamy, czy n jest zerem.
-         jz      .false               ; Jeśli tak, to zwracamy false.
-         mov     rax, 0x80000000      ; Pomocniczo kopiujemy MAXINT + 1 do rax
-         cmp     rdi, rax             ; Sprawdzamy, czy n > MAXINT + 1. Jeśli
-         jg      .false               ; tak, to także zwracamy false.
+         test    rdi, rdi             ; Checking if n==0.
+         jz      .false               ; If so, we return false.
+         mov     rax, 0x80000000      ; Moving value (1<<31) to rax.
+         cmp     rdi, rax             ; Checking if n > (1<<31).
+         jg      .false               ; If so, we return false.
 
-         xor     rcx, rcx             ; rcx będzie naszym iteratorem pętli, więc najpierw go zerujemy 
-         dec     edi                  ; Łatwiej nam będzie pracować na wartości n-1
+         xor     rcx, rcx             ; We will be using rcx as loop iterator so we zero it.
+         dec     edi                  ; n-1 value will be more useful than n later.
 
 .loop_range:                          ; Etykieta pętli, w której będziemy sprawdzać, czy wszystkie elementy tablicy są w zakresie [0, n) JAK.rdi+0x20*rcx
          mov     edx, [rsi+rcx*4]     ; ecx będzie naszym iteratorem.
@@ -53,37 +53,39 @@ inverse_permutation:
          and     [rsi+r10*4], r8d      ; p[j] &= ~(1<<31)
          jmp     .reverse_while          
 
-.false:
-         xor     eax, eax
+.false:                               ; .false label is used to return value representing false.
+         xor     eax, eax             ; rax is now equal to zero.
          ret
 
-.invert_while:                        ; prev_idx = edx, curr_val = eax Które rejestry są już niepotrzebne: r9, r10, r11 
-         mov     r9d, [rsi+rdx*4]     ; r9d = p[prev_idx]
-         test    r9d, r9d
-         cmovs   eax, r9d             ; if (p[prev_idx] < 0) { curr_val = p[prev_idx]; }
-         test    eax, eax
-         js      .nested_while
+.invert_while:                        ; .invert_while label represents while loop that inverts the permutation. 
+                                      ; edx - current index, eax - value to put into p array.
+         mov     r9d, [rsi+rdx*4]     ; Copying p[edx] into r9d register.
+         test    r9d, r9d             ; Checking if p[edx] < 0.
+         cmovs   eax, r9d             ; If so, eax = p[edx].
+         test    eax, eax             ; Checking if p[edx] < 0.
+         js      .nested_while        ; If so, we jump to another while loop "nested" inside our current while loop.
 
-.return_to_outer_while:
-         inc     edx
-         cmp     edi, edx             ; Sprawdzamy, czy edx = prev_idx == n-1
-         jge     .invert_while
-         jmp     .true
+.return_to_outer_while:               ; .return_to_outer_while label represents code that belongs to invert_while but is after nested while loop.
+         inc     edx                  ; Increasing edx register by one.
+         cmp     edi, edx             ; Checking if edx = n-1.
+         jge     .invert_while        ; If so, we jump into the beginning of loop.
+         jmp     .true                ; If not, we return true.
 
-.nested_while:                        ; prev_idx = edx, curr_val = eax, r9 useless
-         test    eax, eax
-         jns     .return_to_outer_while
-         and     eax, r8d             ; curr_val = eax = eax & ~(1<<31) = r8d
-         xor     r9, r9
-         mov     r9d, [rsi+rax*4]      ; r9d = tmp = p[eax] = p[curr_val]
-         test    r9d, r9d            ; if (tmp >= 0) { break; }
-         jns     .return_to_outer_while
-         mov     [rsi+rax*4], edx     ; p[curr_val] = p[eax] = prev_idx = edx
-         mov     edx, eax             ; prev_idx = edx = curr_idx = eax
-         mov     eax, r9d             ; curr_val = tmp
-         jmp     .nested_while
+.nested_while:                        ; .nested_while label represents while loop nested inside invert_while loop.
+                                      ; r9d - helper variable.
+         test    eax, eax             ; Checking if eax >= 0.
+         jns     .return_to_outer_while ; If so, we jump over nested while loop.
+         and     eax, r8d             ; Executing arithmetic operation eax := eax & ~(1<<31).
+         xor     r9, r9               ; Assigning r9 to 0.
+         mov     r9d, [rsi+rax*4]     ; Assigning r9d to p[eax].
+         test    r9d, r9d             ; Checking if r9d >= 0.
+         jns     .return_to_outer_while ; If so, we jump to the rest of the code of outer loop.
+         mov     [rsi+rax*4], edx     ; Assigning p[eax] to edx.
+         mov     edx, eax             ; Assigning edx to eax.
+         mov     eax, r9d             ; Assigning eax = r9d.
+         jmp     .nested_while        ; Jumping to the beginning of nested loop.
 
-.true:
-  lea    rax, [0x1]
-  ret             
+.true:                                ; .true label is used to return value representing true.
+  lea    rax, [0x1]                   ; Assigning rax to 1;
+  ret                                 
          
